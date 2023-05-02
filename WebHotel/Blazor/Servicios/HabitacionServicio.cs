c2d12f10b1f3d024e50ccddcd47fea025eb2eb9f
Usiel Alejandro Pastrana Muñoz <usiel.pastrana@unah.hn>
2023-05-02 00:07:38 -0600

    Servicio Habitaciones
    Primer commit del apartado servicios(Habitaciones)

---
+++ WebHotel/Blazor/Servicios/HabitacionServicio.cs
@@ -0,0 +1,42 @@
+﻿using Blazor.Interfaces;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using Modelos;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+namespace Blazor.Servicios
+{
+    public class HabitacionServicio : IHabitacionServicio
+    {
+        private readonly Config _configuracion;
+        private IHabitacionRepositorio habitacionRepositorio;
+        public HabitacionServicio(Config config)
+        {
+            _configuracion = config;
+            habitacionRepositorio = new HabitacionRepositorio(config.CadenaConexion);
+        }
+        public async Task<bool> Actualizar(Habitacion habitacion)
+        {
+            return await habitacionRepositorio.Actualizar(habitacion);
+        }
+
+        public async Task<bool> Eliminar(int codigo)
+        {
+            return await habitacionRepositorio.Eliminar(codigo);
+        }
+        public async Task<IEnumerable<Habitacion>> GetLista()
+        {
+            return await habitacionRepositorio.GetLista();
+        }
+
+        public async Task<Habitacion> GetPorCodigo(int codigo)
+        {
+            return await habitacionRepositorio.GetPorCodigo(codigo);
+        }
+
+        public async Task<bool> Nuevo(Habitacion habitacion)
+        {
+            return await habitacionRepositorio.Nuevo(habitacion);
+        }
+    }
+}
