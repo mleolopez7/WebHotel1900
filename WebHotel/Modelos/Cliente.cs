2232f29e17306a64ae48465a3119c2e764d6f167
Reniery-22 <maradiagabrayan7@gmail.com>
2023-05-02 00:56:29 -0600

    Commits Clientes

---
+++ WebHotel/Modelos/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    internal class Cliente
+    {
+        [Required(ErrorMessage = "La identidad es obligatorio")]
+        public string Identidad { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La direccion es obligatorio")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "La correo es obligatorio")]
+        public string? Correo { get; set; }
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        public string Telefono { get; set; }
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(string identidad, string nombre, string telefono, string correo, string direccion)
+        {
+            Identidad = identidad;
+            Nombre = nombre;
+            Telefono = telefono;
+            Correo = correo;
+            Direccion = direccion;
+
+
+        }
+
+    }
+}
