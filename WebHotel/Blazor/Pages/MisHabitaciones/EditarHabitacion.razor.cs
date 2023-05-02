851bf7ac6788dd8a9ccea136fc93cec07cfd0a78
wendycalix10 <124416967+wendycalix10@users.noreply.github.com>
2023-05-02 13:40:59 -0600

    ProyectoHotel
    Servicios Habitaciones.

--- WebHotel/Blazor/Pages/MisHabitaciones/EditarHabitacion.razor.cs
+++ WebHotel/Blazor/Pages/MisHabitaciones/EditarHabitacion.razor.cs
@@ -1,1 +1,94 @@
-﻿
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Blazor.Interfaces;
+using Blazor.Servicios;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Modelos;
+using System;
+
+namespace Blazor.Pages.MisHabitaciones
+{
+    public partial class EditarHabitacion
+    {
+        [Inject] private IHabitacionServicio habitacionServicio { get; set; }
+        private Habitacion hab = new Habitacion();
+        [Inject] private SweetAlertService Swal { get; set; }
+        [Inject] NavigationManager _navigationManager { get; set; }
+
+        [Parameter] public string Codigo { get; set; }
+        string imgUrl = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (!string.IsNullOrEmpty(Codigo))
+            {
+                hab = await habitacionServicio.GetPorCodigo(Convert.ToInt32(Codigo));
+            }
+        }
+
+        private async Task SeleccionarImagen(InputFileChangeEventArgs e)
+        {
+            IBrowserFile imgFile = e.File;
+            var buffers = new byte[imgFile.Size];
+            hab.Imagen = buffers;
+            await imgFile.OpenReadStream().ReadAsync(buffers);
+            string imageType = imgFile.ContentType;
+            imgUrl = $"data:{imageType};base64,{Convert.ToBase64String(buffers)}";
+        }
+        protected async Task Guardar()
+        {
+            if (string.IsNullOrEmpty(hab.Descripcion))
+            {
+                return;
+            }
+
+            bool edito = await habitacionServicio.Actualizar(hab);
+            if (edito)
+            {
+                await Swal.FireAsync("Felicidades", "Habitacion guardada con exito", SweetAlertIcon.Success);
+                _navigationManager.NavigateTo("/Habitaciones");
+            }
+            else
+            {
+                await Swal.FireAsync("Error", "Habitacion no se pudo guardar", SweetAlertIcon.Error);
+            }
+        }
+        protected void Cancelar()
+        {
+            _navigationManager.NavigateTo("/Habitaciones");
+        }
+
+        protected async Task Eliminar()
+        {
+            {
+                bool elimino = false;
+
+                SweetAlertResult result = await Swal.FireAsync(new SweetAlertOptions
+                {
+                    Title = "¿Seguro que desea eliminar el registro?",
+                    Icon = SweetAlertIcon.Question,
+                    ShowCancelButton = true,
+                    ConfirmButtonText = "Aceptar",
+                    CancelButtonText = "Cancelar"
+                });
+
+                if (!string.IsNullOrEmpty(result.Value))
+                {
+                    elimino = await habitacionServicio.Eliminar(Convert.ToInt32(Codigo));
+
+                    if (elimino)
+                    {
+                        await Swal.FireAsync("Felicidades", "Habitacion Eliminada", SweetAlertIcon.Success);
+                        _navigationManager.NavigateTo("/Habitaciones");
+                    }
+                    else
+                    {
+                        await Swal.FireAsync("Error", "No se puede eliminar la habitacion", SweetAlertIcon.Error);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- WebHotel/Blazor/Pages/MisHabitaciones/NuevaHabitacion.razor.cs
+++ WebHotel/Blazor/Pages/MisHabitaciones/NuevaHabitacion.razor.cs
@@ -1,1 +1,67 @@
-﻿
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Blazor.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Modelos;
+
+namespace Blazor.Pages.MisHabitaciones
+{
+    public partial class NuevaHabitacion
+    {
+        private Habitacion hab = new Habitacion();
+        [Inject] private IHabitacionServicio habitacionServicio { get; set; }
+
+        [Inject] private SweetAlertService Swal { get; set; }
+        [Inject] NavigationManager navigationManager { get; set; }
+
+        string imgUrl = string.Empty;
+
+        private async Task SeleccionarImagen(InputFileChangeEventArgs e)
+        {
+            IBrowserFile imgFile = e.File;
+            var buffers = new byte[imgFile.Size];
+            hab.Imagen = buffers;
+            await imgFile.OpenReadStream().ReadAsync(buffers);
+            string imageType = imgFile.ContentType;
+            imgUrl = $"data:{imageType};base64,{Convert.ToBase64String(buffers)}";
+        }
+
+        protected async Task Guardar()
+        {
+            if (string.IsNullOrEmpty(hab.Codigo.ToString()) || string.IsNullOrEmpty(hab.Descripcion))
+            {
+                return;
+            }
+            hab.FechaCreacion = DateTime.Now;
+            Habitacion habitacionExistente = new Habitacion();
+
+            habitacionExistente = await habitacionServicio.GetPorCodigo(hab.Codigo);
+
+            if (habitacionExistente != null)
+            {
+                if (habitacionExistente.Codigo != 0)
+                {
+                    await Swal.FireAsync("Advertencia", "Ya existe una habitacion con este codigo", SweetAlertIcon.Warning);
+                    return;
+                }
+            }
+
+            bool inserto = await habitacionServicio.Nuevo(hab);
+
+            if (inserto)
+            {
+                await Swal.FireAsync("Felicidades", "Habitacion guardada con exito", SweetAlertIcon.Success);
+                navigationManager.NavigateTo("/Habitaciones");
+            }
+            else
+            {
+                await Swal.FireAsync("Error", "Habitacion no se pudo guardar", SweetAlertIcon.Error);
+            }
+        }
+        protected async Task Cancelar()
+        {
+            await Swal.FireAsync("Advertencia", "Operacion cancelada con exito", SweetAlertIcon.Success);
+            navigationManager.NavigateTo("/Habitaciones");
+        }
+    }
+}
