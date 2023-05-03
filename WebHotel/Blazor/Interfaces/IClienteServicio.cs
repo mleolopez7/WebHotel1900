f39f41811e6e4da0abfa2f4dac15602e804bbe00
Reniery-22 <maradiagabrayan7@gmail.com>
2023-05-02 18:10:39 -0600

    Commits Clientes

---
+++ WebHotel/Blazor/Interfaces/IClienteServicio.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.Interfaces
+{
+    public class IClienteServicio
+    {
+        Task<Cliente> GetPorCodigo(string identidad);
+        Task<bool> Nuevo(Cliente cliente);
+        Task<bool> Actualizar(Cliente cliente);
+        Task<bool> Eliminar(string identidad);
+        Task<IEnumerable<Cliente>> GetLista();
+    }   
+}
